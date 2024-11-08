6775a72b88dcc22266f781f7265df493cdcb4ad0
Piyush Hinduja <74929365+piyushinduja@users.noreply.github.com>
2024-11-07 21:39:18 -0700

    Update profile.py

--- profile.py
+++ profile.py
@@ -4,6 +4,10 @@ import geni.rspec.pg as pg
 # Create a portal context and define parameters
 pc = portal.Context()
 pc.defineParameter("nodeCount", "Number of Nodes", portal.ParameterType.INTEGER, 1)
+pc.defineParameter("phystype",  "Optional physical node type",
+                   portal.ParameterType.NODETYPE, "",
+                   longDescription="Pick a single physical node type (pc3000,d710,etc) " +
+                   "instead of letting the resource mapper choose for you.")
 params = pc.bindParameters()
 
 # Create a Request object
@@ -13,7 +17,7 @@ request = pc.makeRequestRSpec()
 for i in range(params.nodeCount):
     node = request.RawPC("node" + str(i))
     node.disk_image = "urn:publicid:IDN+wisc.cloudlab.us+image+distribml-PG0:python-setup.node0-nvidia-cuda"
-    node.addService(pg.Execute(shell="sh", command="/local/repository/setup.sh"))
+    # node.addService(pg.Execute(shell="sh", command="/local/repository/setup.sh"))
 
 # Output the request RSpec
 pc.printRequestRSpec(request)
